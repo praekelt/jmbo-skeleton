4d958f4b7810c30ba178df515f32d98921db7ad9
hedleyroos <hedleyroos@gmail.com>
2012-08-02 19:23:34 +0200

    Make 1.4 compatible

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='',
     packages = find_packages(),
     install_requires = [
-        'jmbo-foundry>=0.5',
+        'jmbo-foundry>=0.5.1',
     ],
     include_package_data=True,
     tests_require=[
