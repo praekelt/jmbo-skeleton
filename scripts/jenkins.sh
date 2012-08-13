c1dd6d4ef6ed7ae87e2259c7a3d7fa25fdb5892a
hedleyroos <hedleyroos@gmail.com>
2012-08-13 15:31:41 +0200

    Jenkins 1

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -6,14 +6,14 @@ SITE_TYPE=basic
 virtualenv --no-site-packages ve
 ve/bin/python bootstrap.py
 
-./bin/buildout -c dev_${SITE_TYPE}_site.cfg
+bin/buildout -c dev_${SITE_TYPE}_site.cfg
 
-./bin/skeleton-dev-$SITE_TYPE-site syncdb
-./bin/skeleton-dev-$SITE_TYPE-site migrate
-./bin/skeleton-dev-$SITE_TYPE-site load_photosizes
-./bin/skeleton-dev-$SITE_TYPE-site loaddata skeleton/fixtures/sites.json
+bin/skeleton-dev-$SITE_TYPE-site syncdb
+bin/skeleton-dev-$SITE_TYPE-site migrate
+bin/skeleton-dev-$SITE_TYPE-site load_photosizes
+bin/skeleton-dev-$SITE_TYPE-site loaddata skeleton/fixtures/sites.json
 #rm -rf static
-./bin/skeleton-dev-$SITE_TYPE-site collectstatic --noinput
+bin/skeleton-dev-$SITE_TYPE-site collectstatic --noinput
 
 cd src/jmbo-foundry
 ../bin/setuptest-runner setup.py test
