b1b651f8cd3f377cb3c68f621ff38005e3456e6b
hedleyroos <hedleyroos@gmail.com>
2013-04-03 21:31:58 +0200

    Do buildout in /tmp to speed up deploys

--- scripts/deploy-project.sh
+++ scripts/deploy-project.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 # Add new sites or upgrade them if they already exist. This script is idempotent.
-#
-# There are levels of operation. Level 1 is quickest since it just updates the
-# code and restarts. Level 3 is slowest since it cleans out existing code and
-# does a buildout and restart.
-#
-# 1: Update source code. No buildout.
-# 2: Update source code. Buildout.
-# 3: Remove existing source code and update. Buildout.
+
 
 # Default values
 CREDENTIALS=praekeltdeploy:password
 USER=www-data
-LEVEL=1
 
 # Parse arguments
 while getopts "p:d:r:b:c:u:l:" opt; do
@@ -30,14 +22,12 @@ while getopts "p:d:r:b:c:u:l:" opt; do
             CREDENTIALS=$OPTARG;;
         u)
             USER=$OPTARG;;
-        l)
-            LEVEL=$OPTARG;;
     esac    
 done
 
 if [[ -z "$PREFIX" || -z "$DEPLOY_TYPE" || -z "$OWNER_AND_REPO" || -z "$BRANCH" || -z "$CREDENTIALS" ]];
 then
-    echo "Usage: deploy-project.sh -p (prefix) -d (deploy_type) -r (repo) -b (branch) -c (credentials) [-u (user) -l (level)]"
+    echo "Usage: deploy-project.sh -p (prefix) -d (deploy_type) -r (repo) -b (branch) -c (credentials) [-u (user)]"
     echo "Example: deploy-project.sh -p praekelt -d qa -r praekelt/jmbo-foo -b develop -c praekeltdeploy:mypassword"
     exit 1
 fi
@@ -51,8 +41,13 @@ REPO=${OWNER_AND_REPO:${INDEX}}
 INDEX=`expr index "$REPO" [-.]`
 APP_NAME=${REPO:${INDEX}}
 
-# Compute deploy directory
+# Compute deploy and working directory
 DEPLOY_DIR=/var/${REPO_OWNER}
+WORKING_DIR=/tmp/${REPO_OWNER}
+
+# Ensure working directory is clean
+sudo rm -rf $WORKING_DIR
+sudo -u $USER mkdir $WORKING_DIR
 
 # Checkout / update repo to /tmp
 cd /tmp
@@ -63,8 +58,6 @@ if [ -d $REPO ]; then
 else
     sudo -u $USER git clone -b $BRANCH https://${CREDENTIALS}@github.com/$REPO_OWNER/$REPO.git
 fi
-
-sudo supervisorctl stop all
 
 # Create database. Safe to run even if database already exists.
 IS_NEW_DATABASE=0
@@ -88,42 +81,26 @@ do
         FTMP=${FILENAME%.*}
         THEDIR=$PREFIX-${FTMP//_/-}
 
-        # Clone, bootstrap, buildout
-        cd ${DEPLOY_DIR}/
-        sudo -u $USER mkdir ${DEPLOY_DIR}/static-backups
-
         # Backup existing static directory if it exists
-        if [ -d ${THEDIR}/static ]; then
+        sudo -u $USER mkdir ${DEPLOY_DIR}/static-backups
+        if [ -d ${DEPLOY_DIR}/${THEDIR}/static ]; then
             ADATE=`date +"%Y%m%dT%H%M"`
             STATIC_BACKUP=${DEPLOY_DIR}/static-backups/${THEDIR}/${ADATE}
             sudo -u $USER mkdir -p $STATIC_BACKUP
-            sudo -u $USER cp -r ${THEDIR}/static ${STATIC_BACKUP}/
+            sudo -u $USER cp -r ${DEPLOY_DIR}/${THEDIR}/static ${STATIC_BACKUP}/
         fi
 
-        # Nuke source when level 3
-        if [ $LEVEL == 3 ]; then
-            sudo -u $USER rm -rf $THEDIR
-        fi
-
-        IS_NEW=0
-        if [ -d $THEDIR ]; then
-            cd $THEDIR
-            sudo -u $USER git checkout $BRANCH
-            sudo -u $USER git pull
-        else
-            IS_NEW=1
-            sudo -u $USER git clone -b $BRANCH https://${CREDENTIALS}@github.com/$REPO_OWNER/$REPO.git $THEDIR
-            cd $THEDIR
-            sudo chown -R $USER:$USER .git/
-        fi
+        # Checkout
+        cd ${WORKING_DIR}/
+        sudo -u $USER git clone -b $BRANCH https://${CREDENTIALS}@github.com/$REPO_OWNER/$REPO.git /tmp/${PREFIX}/${THEDIR}
+        cd /tmp/${PREFIX}/${THEDIR}
+        sudo chown -R $USER:$USER .git/
 
         # Always re-bootstrap in case of a new version of distribute
-        sudo -u $USER ../python/bin/python bootstrap.py -v 1.7.0
+        sudo -u $USER ${DEPLOY_DIR}/python/bin/python bootstrap.py -v 1.7.0
 
-        if [[ $IS_NEW == 1 || $LEVEL -ge 2 ]]; then
-            # Must use -i so buildout cache is used. That necessitates full paths as arguments.
-            sudo -u $USER -i ${DEPLOY_DIR}/${THEDIR}/bin/buildout -Nv -c ${DEPLOY_DIR}/${THEDIR}/$FILENAME
-        fi
+        # Must use -i so buildout cache is used. That necessitates full paths as arguments.
+        sudo -u $USER -i ${PREFIX}/${THEDIR}/bin/buildout -Nv -c ${PREFIX}/${THEDIR}/$FILENAME
 
         if [[ $FILENAME != *_common_*.cfg ]]; then
 
@@ -171,7 +148,7 @@ do
             for COMMAND in report_naughty_words jmbo_publish; do
                 RESULT=`grep "${THEDIR} ${COMMAND}" /tmp/acron`
                 if [ "$RESULT" == "" ]; then
-                    echo "0 * * * * ${DEPLOY_DIR}/${THEDIR}/bin/${THEDIR} ${COMMAND}" >> /tmp/acron
+                    echo "0 * * * * ${WORKING_DIR}/${THEDIR}/bin/${THEDIR} ${COMMAND}" >> /tmp/acron
                 fi
             done
             sudo -u $USER crontab /tmp/acron
@@ -181,16 +158,26 @@ do
         fi
         
         # Create nginx symlink if required
-        if [ -d ${DEPLOY_DIR}/${THEDIR}/nginx ]; then
-            sudo ln -s ${DEPLOY_DIR}/${THEDIR}/nginx/* /etc/nginx/sites-enabled/
+        if [ -d ${WORKING_DIR}/${THEDIR}/nginx ]; then
+            sudo ln -s ${WORKING_DIR}/${THEDIR}/nginx/* /etc/nginx/sites-enabled/
         fi
 
         # Create supervisor symlink if required
-        if [ -d ${DEPLOY_DIR}/${THEDIR}/supervisor ]; then
-            sudo ln -s ${DEPLOY_DIR}/${THEDIR}/supervisor/* /etc/supervisor/conf.d/
+        if [ -d ${WORKING_DIR}/${THEDIR}/supervisor ]; then
+            sudo ln -s ${WORKING_DIR}/${THEDIR}/supervisor/* /etc/supervisor/conf.d/
         fi
 
     fi
+done
+
+# Move directories in working directory to deploy directory
+for f in `ls $WORKING_DIR`
+do
+    # Delete target if it exists
+    if [ -d ${DEPLOY_DIR}/${f} ]; then
+        sudo -u $USER rm -rf ${DEPLOY_DIR}/${f}
+    fi
+    sudo -u $USER mv ${WORKING_DIR}/${}f $DEPLOY_DIR/
 done
 
 # Update supervisor
@@ -199,7 +186,9 @@ sudo supervisorctl update
 # Restart memcached
 sudo /etc/init.d/memcached restart
 
-sudo supervisorctl start all
+# Restart processes
+# todo: make finer grained
+sudo supervisorctl restart all
 
 # Reload nginx
 sudo /etc/init.d/nginx reload
