6e4ca5934d0830cb1c26a1d21cca734c61ea080a
hedleyroos <hedleyroos@gmail.com>
2013-04-17 15:16:09 +0200

    Abort on buildout failure

--- scripts/deploy-project.sh
+++ scripts/deploy-project.sh
@@ -105,6 +105,12 @@ do
         # Must use -i so buildout cache is used. That necessitates full paths as arguments.
         sudo -u $USER -i ${WORKING_DIR}/${THEDIR}/bin/buildout -Nv -c ${WORKING_DIR}/${THEDIR}/$FILENAME
 
+        # Checking for the eggs directory is a good way to detect failure.
+        if [ ! -d ${WORKING_DIR}/${THEDIR}/eggs ]; then
+            echo "Buildout failure. Aborting."
+            exit 1
+        fi
+
         if [[ $FILENAME != *_common_*.cfg ]]; then
 
             # Database setup on first loop
