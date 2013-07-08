aea4f7f2606a0d4a6da143cd8174312d1673d70b
hedleyroos <hedleyroos@gmail.com>
2013-07-08 16:07:37 +0200

    Use postgis

--- test_settings.py
+++ test_settings.py
@@ -3,13 +3,13 @@ from skeleton.settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'skeleton',
-        'USER': '',
+        'USER': 'test',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
 }
 
-# Need this last line until django-setuptest is improved. 
+# Need this last line until django-setuptest is improved.
