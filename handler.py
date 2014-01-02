19f8d8156717d647c23fb6628b8275ef24132320
hedleyroos <hedleyroos@gmail.com>
2014-01-02 12:06:41 +0200

    Make handler resistant to failure

--- handler.py
+++ handler.py
@@ -1,6 +1,11 @@
-from twisted.internet.defer import inlineCallbacks, returnValue
+import base64
+import json
 
-from devproxy.handlers.wurfl_handler.base import WurflHandler
+from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.error import ConnectError
+
+from twisted.web.client import getPage
+from devproxy.handlers.wurfl_handler.base import WurflHandler, WurflHandlerException
 from devproxy.handlers.wurfl_handler.scientia_mobile_cloud \
     import ScientiaMobileCloudHandler
 
@@ -80,12 +85,26 @@ class MobiHandler(WurflHandler):
         return [result]
 
 
+class ScientiaMobileCloudHandlerConnectError(Exception):
+    pass
+
+
 class ScientiaMobileCloudResolutionHandler(ScientiaMobileCloudHandler):
-    """This needs work to be more like the three classes above"""
+    """todo: contribute handle_request_and_cache and get_device_from_smcloud
+    back to deviceproxy"""
 
     @inlineCallbacks
     def get_headers(self, request):
         user_agent = unicode(request.getHeader('User-Agent') or '')
+
+        # Handling for devices not yet recognized by the Wurfl service.
+        # Return early if this is a bot that should not be redirected.
+        if 'PageFetcher-Google-CoOp' in user_agent:
+            # When X-Site-Type is 'mobi', default to 'basic'
+            site_type = request.getHeader('X-Site-Type')
+            layer = 'web' if site_type == 'web' else 'basic'
+            returnValue([{self.header_name: layer}])
+
         cache_key = self.get_cache_key(user_agent)
         flags, cached = yield self.memcached.get(cache_key)
         if cached:
@@ -122,14 +141,54 @@ class ScientiaMobileCloudResolutionHandler(ScientiaMobileCloudHandler):
 
         returnValue(headers)
 
+    @inlineCallbacks
+    def handle_request_and_cache(self, cache_key, user_agent, request):
+        expireTime = self.cache_lifetime
+        try:
+            device = yield self.get_device_from_smcloud(user_agent)
+        except ScientiaMobileCloudHandlerConnectError:
+            device = {}
+            expireTime = 60
+        headers = self.handle_device(request, device)
+        yield self.memcached.set(cache_key, json.dumps(headers),
+                                 expireTime=expireTime)
+        returnValue(headers)
+
+    @inlineCallbacks
+    def get_device_from_smcloud(self, user_agent):
+        """
+        Queries ScientiaMobile's API and returns a dictionary of the device.
+        """
+        # create basic auth string
+        b64 = base64.encodestring(self.smcloud_api_key).strip()
+        headers = {
+            #User-Agent is set by agent in getPage.
+            'X-Cloud-Client': self.SMCLOUD_CONFIG['client_version'],
+            'Authorization': 'Basic %s' % b64
+        }
+        try:
+            page = yield getPage(self.SMCLOUD_CONFIG['url'], headers=headers,
+                             agent=user_agent, timeout=5)
+        except ConnectError, exc:
+            raise ScientiaMobileCloudHandlerConnectError(exc)
+        device = json.loads(page)
+        returnValue(device)
+
     def handle_device(self, request, device):
         result = {
-            'is_web_browser': device['capabilities']['is_full_desktop'] and '1' or '',
+            'is_web_browser': '1',
             self.header_name: 'basic'
         }
 
-        if (device['capabilities']['resolution_width'] >= 320) \
-            and (device['capabilities']['pointing_method'] == 'touchscreen'):
-            result[self.header_name] = 'smart'
+        try:
+            result = {
+                'is_web_browser': (device['capabilities']['ux_full_desktop'] or device['capabilities']['is_tablet']) and '1' or '',
+                self.header_name: 'basic'
+            }
+            if (device['capabilities']['resolution_width'] >= 320) \
+                and (device['capabilities']['pointing_method'] == 'touchscreen'):
+                result[self.header_name] = 'smart'
+        except KeyError:
+            pass
 
         return [result]
