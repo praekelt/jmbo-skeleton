6a47ee827c27aeef9757287a623d275acd3837b8
hedleyroos <hedleyroos@gmail.com>
2015-06-30 10:51:55 +0200

    Use redis for queues

--- project/settings.py
+++ project/settings.py
@@ -352,6 +352,9 @@ CACHES = {
     }
 }
 
+BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+
 # Debug toolbar. Uncomment if required.
 #INSTALLED_APPS += ('debug_toolbar',)
 #MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
